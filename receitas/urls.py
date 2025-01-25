3395d857c5c6f2af1ed4ac2a6937034bab3f4ac7
Jherfson Castro <jherfson.castro@gmail.com>
2025-01-25 10:42:44 -0300

    Realizando exclusão dos testes

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from receitas.views import home, contato, sobre
+from receitas.views import home
 
 urlpatterns = [
     path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre)
+
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'receitas/home.html')
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
