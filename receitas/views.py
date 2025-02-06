fd82b6f7977248eac2e87b5ad6fa546dae7bc82f
Jherfson Castro <jherfson.castro@gmail.com>
2025-02-05 21:06:07 -0300

    add views.py

--- receitas/views.py
+++ receitas/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'receitas/pages/home.html')
+
+
+def recipe(request, id):
+    return render(request, 'receitas/pages/home.html')
